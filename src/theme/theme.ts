87c88cff3e6831934e7c303cb3868e8e140b1b14
abhijith-p-subash <abhijith.p.subash@gmail.com>
2024-11-25 15:40:42 +0530

    theme update

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -108,7 +108,7 @@ export const customTheme: CustomFlowbiteTheme = {
       on: "rounded-full",
     },
     size: {
-      xs: "px-2 py-1 text-xs",
+      xs: "px-1 py-1 text-xs",
       sm: "px-3 py-1.5 text-sm",
       md: "px-4 py-2 text-sm",
       lg: "px-5 py-2.5 text-base",
